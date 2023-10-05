b370968811e0689062648bebc9df3b744c73417d
SaydsC <G00411297@gmit.ie>
2023-10-05 14:09:46 +0100

    lab1

--- Labs/traintimes.py
+++ Labs/traintimes.py
@@ -17,4 +17,8 @@ page = requests.get(url)
 
 doc = parseString(page.content)
 #check it works
-print (doc.toprettyxml()) #output to console
+# print (doc.toprettyxml()) #output to console
+
+# if I want to store the xml in a file
+with open("trainxml.xml","w") as xmlfp:
+    doc.writexml(xmlfp)
